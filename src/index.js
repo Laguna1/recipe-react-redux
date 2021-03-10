bbd7f5e6b5c4398e72081d01288333c4a8498a36
Laguna1 <lagunazoomarket@gmail.com>
2021-03-10 23:35:27 +0200

    Add Footer and styles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+// eslint-disable-next-line import/extensions
+import Footer from './layouts/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/layouts/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="footer-left">
+      <p>
+        Using
+        <span className="name"> TheMealDB</span>
+      </p>
+    </div>
+    <div className="footer-right">
+      <span>About</span>
+      <span>Faq</span>
+      <span>Contacts</span>
+    </div>
+  </div>
+);
+
+export default Footer;
